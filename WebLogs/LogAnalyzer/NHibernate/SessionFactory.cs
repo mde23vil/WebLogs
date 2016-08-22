40191286b7294cd73ae650e885cdede04d15250d
mde23vil <hydrt@yahoo.com>
2016-08-22 20:54:16 +0400

    Added the 3d button to home page

--- WebLogs/LogAnalyzer/NHibernate/SessionFactory.cs
+++ WebLogs/LogAnalyzer/NHibernate/SessionFactory.cs
@@ -15,7 +15,7 @@ namespace LogAnalyzer.NHibernate
     private static string _connectionString;
     private static ISessionFactory _sessionFactory;
 
-    private static ISessionFactory CreateSessionFactory(string connectionString = @"Data Source = Ruslan-PC2\SQLEXPRESS; Initial Catalog = LogAnalyzer; User ID = admin; Password = 11111")
+    private static ISessionFactory CreateSessionFactory(string connectionString = @"Data Source = DESKTOP-M9RDV9L\SQLEXPRESS; Initial Catalog = LogAnalyzer; User ID = admin; Password = 11111")
     {
       _connectionString = connectionString;
       return Fluently
