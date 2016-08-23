73fdc3720c23962bd075f2a9d0db22c656ed7999
mde23vil <hydrt@yahoo.com>
2016-08-23 22:31:39 +0400

    Localization: Culture attribute, that loads language code from cookies

--- WebLogs/LogAnalyzer/Controllers/HomeController.cs
+++ WebLogs/LogAnalyzer/Controllers/HomeController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LogAnalyzer.Filters;
 
 namespace LogAnalyzer.Controllers
 {
+  [Culture]
   public class HomeController : Controller
   {
     public ActionResult Index()
---
+++ WebLogs/LogAnalyzer/Controllers/ServiceController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LogAnalyzer.Controllers
+{
+  public class ServiceController : Controller
+  {
+    // GET: Service
+    public ActionResult ChangeCulture(string lang)
+    {
+      string returnUrl = Request.UrlReferrer.AbsolutePath;
+      HttpCookie cookie = Request.Cookies["lang"];
+      List<string> availableCultures = new List<string>() { "ru", "en" };
+
+      if (!availableCultures.Contains(lang))
+      {
+        lang = "ru";
+      }
+
+      if (cookie != null)
+      {
+        cookie.Value = lang;
+      }
+      else
+      {
+        cookie = new HttpCookie("lang")
+        {
+          HttpOnly = false,
+          Value = lang,
+          Expires = DateTime.Now.AddYears(1)
+        };
+      }
+
+      Response.Cookies.Add(cookie);
+      return Redirect(returnUrl);
+    }
+  }
+}
